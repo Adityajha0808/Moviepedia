8a1b4d25b50134c6f5a81483b78c9a5a3e8eb754
Aditya Nath Jha <jhaaditya757@gmail.com>
2021-09-04 01:05:19 +0530

    Add files via upload

---
+++ client/src/components/Config.js
@@ -0,0 +1,14 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = '885b3551f2d95f7ffd308c7e5ba33d63';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+export const BACKDROP_SIZE = 'w1280'
+
+export const IMAGE_SIZE = 'w1280'
+
+export const POSTER_SIZE = 'w500'
